e9b43c9e211aaaf07ab06449d9ac8bf02027a68e
Eliot Reyes <eliotmij@gmail.com>
2016-08-10 21:16:35 -0700

    date month fix

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -108,7 +108,7 @@ var tempDateArray = obj.startDate.split('-');
 function monthDiff(d1, d2) {
    var months;
    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-   months -= d1.getMonth() + 1;
+   months -= d1.getMonth();
    months += d2.getMonth();
    return months <= 0 ? 0 : months;
 }
